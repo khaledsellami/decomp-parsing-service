fdb1f84d111c47d2b681a0aa8988e77f80d44a52
khalsel <khaled.sellami.1@ulaval.ca>
2024-03-10 16:20:32 -0400

    analysis jar

--- decparsing/config.py
+++ decparsing/config.py
@@ -2,4 +2,4 @@ import os
 
 
 RESTRICT_APPS = False
-DEFAULT_JAR_PATH = os.path.join(os.path.dirname(__file__), "analysis", "java-analysis-service.jar")
+DEFAULT_JAR_PATH = os.path.join(os.path.dirname(__file__), "analysis", "resources", "java-analysis-service-1-3-0.jar")
